87dded15d925e2e878ee304c68015b7cef76652e
aman-av <itz.aman.av@gmail.com>
2023-04-30 08:59:56 +0530

    cors path added

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use(
 app.use(bodyParser.json());
 app.use(
 	  	cors({
-			    origin: ["http://localhost:3000",'https://savorshare.vercel.app/'], 
+			    origin: ["http://localhost:3000",'https://savorshare.vercel.app/','http://savorshare-hv3usqgvl-vishalbrdr.vercel.app'], 
 		    	methods: [ "GET", "POST","DELETE" ],
 		    	credentials: true,
 		  	})
